c2ca2a68406b614f7ede76ebf09045cd22917c9b
Peter Bräuer <pb866@york.ac.uk>
2018-11-29 14:49:20 +0000

    Auto-generate TUV input files
    Add package Printf in Project.toml.
    New and updated functions to write to autogenerate TUV input files for
    the TUV rxn files and the files with the subroutine calls.
    Choose options to leave flags untouched (negative setflags), set all
    flags to F (setflags = 0), T (setflags = 1) or set the reactions used in
    MCM/GECKO-A to T (setflags = 2).

--- src/TUVtools.jl
+++ src/TUVtools.jl
@@ -1,12 +1,23 @@
 module TUVtools
 
 using filehandling
+using Printf
 
 export setrxns
 
-function setrxns(tuvdir::String; inputfile::Union{String,Vector{String}}="usrinp",
-  setflags::Union{String, Int64}="")
+function setrxns(tuvdir::String; inputfiles::Union{String,Vector{String}}="",
+  setflags::Int64=-1)
+
   currdir = pwd()
+  rxnfiles, callfiles = getfiles(tuvdir)
+  rxnlist = generate_rxns(rxnfiles, callfiles)
+
+  write_rxns(inputfiles, tuvdir, rxnlist, setflags)
+  cd(currdir)
+end #function setrxns
+
+
+function getfiles(tuvdir)
   tuvdir = abspath(tuvdir)
   cd(joinpath(tuvdir, "SRC/RXN"))
   tuvfiles = readdir()
@@ -14,15 +25,32 @@ function setrxns(tuvdir::String; inputfile::Union{String,Vector{String}}="usrinp
   callfiles = deepcopy(tuvfiles)
   deleteat!(callfiles,findall(startswith.(callfiles, "rxn")))
   rxnfiles = setdiff(tuvfiles, callfiles)
+  return rxnfiles, callfiles
+end #function getfiles
 
+function generate_rxns(rxnfiles, callfiles)
+  # Initialise
+  rxnlist = String[]
+
+  # Get list of subroutines and associated reactions
   rxns = findrxnlabel(rxnfiles)
-  callorder = getorder(callfiles)
+  # Get order the subroutines calls = order of reactions in input files
+  order = getorder(callfiles)
 
-  cd(joinpath(tuvdir, "INPUTS"))
-  write_rxns(inputfile, rxns, callorder, setflags)
-  cd(currdir)
+  # Save O2 reaction label from swchem.f
+  lines = readfile("swchem.f")
+  iO2 = findfirst([occursin(r"^[ \t]*jlabel"i, line) for line in lines])
+  O2rxn = replace(lines[iO2], r"^(.*?)(\'|\")" => "")
+  O2rxn = strip(replace(O2rxn, r"(\'|\").*" => ""))
+  push!(rxnlist, O2rxn)
 
-end #function setrxns
+  # Save reaction labels from rxn files in order
+  for SR in order
+    rxnlist = vcat(rxnlist, rxns[SR])
+  end
+  # Return String vector with reaction list
+  return rxnlist
+end
 
 function findrxnlabel(filelist)
   # Initialise dictionary with subroutines and their associated reactions
@@ -37,12 +65,12 @@ function findrxnlabel(filelist)
     irxns = findall([occursin(r"^[ \t]*jlabel"i, line) for line in lines])
 
     # Save subroutine names
-    subs = [replace(line, r"^.*subroutine"i => "") for line in lines[isubs]]
-    subs = [strip(replace(line, r"\(.*"i => "")) for line in subs]
+    # subs = [replace(line, r"^.*subroutine"i => "") for line in lines[isubs]]
+    # subs = [strip(replace(line, r"\(.*"i => "")) for line in subs]
+    subs = [strip(match(r"(?<=subroutine).*?(?=\()"i, rxn).match) for rxn in lines[isubs]]
 
     # Save reaction labels
-    rxns = [replace(rxn, r"^(.*?)(\'|\")" => "") for rxn in lines[irxns]]
-    rxns = [strip(replace(rxn, r"(\'|\").*" => "")) for rxn in rxns]
+    rxns = [match(r"(?<=\"|\').*(?=\"|\')"i, rxn).match for rxn in lines[irxns]]
 
     # Add index for end of file
     push!(isubs, length(lines))
@@ -60,6 +88,7 @@ end #function findrxnlabel
 function getorder(filelist)
   # Initialise array with order of calls
   callorder = String[]
+
   # Loop over input files in reverse alphabetical order (as they are called in TUV)
   for file in reverse(sort(filelist))
     # Read files
@@ -73,7 +102,76 @@ function getorder(filelist)
   end
 
   return callorder
-end
+end #function getorder #function generate_rxns
 
+function write_rxns(filelist, tuvdir, rxnlist, setflags)
+  cd(joinpath(tuvdir, "INPUTS"))
+  flags = Vector{Char}(undef, length(rxnlist))
+  if setflags == 0
+    flags .= 'F'
+  elseif setflags == 1
+    flags .= 'T'
+  elseif setflags > 1
+    if setflags == 2
+      mcm = read_data(joinpath(@__DIR__, "data/MCM-GECKO-A.db"), sep = "|",
+      headerskip = 1, colnames = ["number", "label"])
+    elseif setflags == 3
+      mcm = read_data(joinpath(@__DIR__, "data/MCMv331.db"), sep = "|",
+      headerskip = 1, colnames = ["number", "label"])
+    end
+    flags = Vector{Char}(undef, length(rxnlist))
+    flags .= 'F'
+    fail = String[]
+    for rxn in mcm[:label]
+      idx = findfirst(rxnlist.==rxn)
+      if idx == nothing
+        push!(fail, rxn)
+      else
+        flags[idx] = 'T'
+      end
+    end
+    if length(fail) > 0
+      println(
+        "\033[95mWarning! $counter reactions not found in MCM database:\n\33[0m",
+        join(fail, "\n"))
+    end
+  end
+  if filelist == ""
+    filelist = readdir()
+    deleteat!(filelist,findall(startswith.(filelist, ".")))
+  elseif filelist isa String
+    filelist = [filelist]
+  end
+  for file in filelist
+    lines = readfile(file)
+    istart = findfirst(occursin.("photolysis reactions", lines))
+    iend   = findlast(startswith.(lines, "==="))
+    inmj   = findfirst(occursin.("nmj", lines))
+
+    if setflags < 0
+      flags = [line[1]  for line in lines[istart+1:iend-1]]
+      if length(flags) < length(rxnlist)
+        [push!(flags, 'T') for i = 1:1+length(rxnlist) - length(flags)]
+        println("\033[95mWarning! Less flags in input file defined ",
+          "than needed for current mechanism.\n\33[0m",
+          "Last $(1+length(rxnlist) - length(flags)) flags set to true.")
+      elseif length(flags) > length(rxnlist)
+        println("\033[95mWarning! More flags in input file defined ",
+          "than needed for current mechanism.\n\33[0m",
+          "Last $(1+length(rxnlist) - length(flags)) flags ignored.")
+        deleteat!(flags, 1+length(rxnlist) - length(flags):length(rxnlist))
+      end
+    end
+
+    lines[inmj] = @sprintf("%s%3d", lines[inmj][1:end-3], length(findall(flags.=='T')))
+    open(file, "w+") do f
+      [println(f, line) for line in lines[1:istart]]
+      [@printf(f, "%s%3d %s\n", flags[i], collect(1:length(rxnlist))[i], rxnlist[i])
+        for i = 1:length(rxnlist)]
+      [println(f, line) for line in lines[iend:end]]
+    end
+  end
+
+end #function write_rxns
 
 end # module TUVtools
